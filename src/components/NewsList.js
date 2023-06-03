354b637ff8407cbd306a4479b7d52a419b559024
AndonG7 <andon4ee@gmail.com>
2023-06-03 18:47:16 +0200

    bug fixes

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -11,7 +11,7 @@ function NewsList(props) {
             <>
              {newsData.map((_news, index) => {
                 return (
-                    <div className={`news-card ${index}`} key={index} >
+                    <div className={`news-cardd ${index}`} key={index} >
                         <h4 className='news-author'>{_news.author}</h4>
                         <h2 className='news-title'>{_news.title}</h2>
                         <h4 className='news-url'><a href={_news.url} target="_blank">Read more</a></h4>
