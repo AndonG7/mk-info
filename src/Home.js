a327ce0a1b2523a210a4bb7451cd6676524c0aa2
AndonG7 <andon4ee@gmail.com>
2023-06-08 15:36:39 +0200

    home

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import {React, useState, useEffect} from 'react';
+import React from 'react';
 import Nav from './components/Nav';
 import TopThree from './components/topThree';
 import MoreTrending from './components/MoreTrending';
@@ -8,40 +8,31 @@ import BusinessNews from './components/businessNews';
 import Footer from './components/footer';
 
 function Home(props) {
-  //Top news data
-  const topData = props.topData;
-  //Sport news data
-  const sportData = props.sportData;
-  //Technology news data
-  const technologyData = props.technologyData;
-  //Technology news data
-  const businessData = props.businessData;
+  const { topData, sportData, technologyData, businessData } = props;
+
+  if (!topData || !topData.articles || topData.articles.length < 3) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="Home">
-      {topData && (
-        <>
-          {/* <h1>{data.articles[0].title}</h1> */}
-          <Nav active={"Home"} topHeadlines={[topData.articles[0], topData.articles[1], topData.articles[2]]} />
-          <TopThree ar1={topData.articles[0]} ar2={topData.articles[1]} ar3={topData.articles[2]} />
-          <MoreTrending smallScreen={props.smallScreen} />
-        {sportData && (
-          <SportNews ar1={sportData.articles[0]} ar2={sportData.articles[1]} ar3={sportData.articles[2]} />
-        )}
-        {technologyData && (
-          <TechnologyNews ar1={technologyData.articles[0]} ar2={technologyData.articles[1]} ar3={technologyData.articles[2]}/>
-        )}
-        {businessData && (
-          <BusinessNews ar1={businessData.articles[0]} ar2={businessData.articles[1]} ar3={businessData.articles[2]}/>
-        )}
-        </>
+      <Nav active="Home" topHeadlines={topData.articles.slice(0, 3)} />
+      <TopThree ar1={topData.articles[0]} ar2={topData.articles[1]} ar3={topData.articles[2]} />
+      <MoreTrending smallScreen={props.smallScreen} />
+      
+      {sportData && sportData.articles && sportData.articles.length >= 3 && (
+        <SportNews ar1={sportData.articles[0]} ar2={sportData.articles[1]} ar3={sportData.articles[2]} />
+      )}
+      
+      {technologyData && technologyData.articles && technologyData.articles.length >= 3 && (
+        <TechnologyNews ar1={technologyData.articles[0]} ar2={technologyData.articles[1]} ar3={technologyData.articles[2]} />
+      )}
+      
+      {businessData && businessData.articles && businessData.articles.length >= 3 && (
+        <BusinessNews ar1={businessData.articles[0]} ar2={businessData.articles[1]} ar3={businessData.articles[2]} />
       )}
     </div>
   );
 }
 
 export default Home;
-
-
-
-
